c2f1d9f0270aedb5273da332d71469f69a985b83
Meghan Kozal <mkozal@nd.edu>
2018-10-01 11:06:40 -0400

    P2 files
    all 2nd projects

---
+++ VCDP2/SlickQuiz-master/js/slickQuiz-config.js
@@ -0,0 +1,74 @@
+// Setup your quiz text and questions here
+
+// NOTE: pay attention to commas, IE struggles with those bad boys
+
+var quizJSON = {
+    "info": {
+        "name":    "Test Your Knowledge!!",
+        "main":    "<p>How well did you learn the order of the presidents?</p>",
+        "results": "<h5>To review again</h5>",
+        "level1":  "Presidential Master",
+        "level2":  "A Real Presidential Historian",
+        "level3":  "You are getting there!",
+        "level4":  "You might want to go through the presidents again",
+        "level5":  "Let's try that again" // no comma here
+    },
+    "questions": [
+        { // Question 1 - Multiple Choice, Single True Answer
+            "q": "Who was the first president?",
+            "a": [
+                {"option": "John Adams",      "correct": false},
+                {"option": "Thomas Jefferson",     "correct": false},
+                {"option": "George Washington",      "correct": true},
+                {"option": "James Madison",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> </p>",
+            "incorrect": "<p><span>No, sorry</span>Remember <strong> Wash a ton</strong> for Washington</p>" // no comma here
+        },
+        { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
+            "q": "Who came after James Madison?",
+            "a": [
+                {"option": "John Adams",               "correct": false},
+                {"option": "James Monroe",   "correct": true},
+                {"option": "John Q Adams",               "correct": false},
+                {"option": "Andrew Jackson", "correct": false} // no comma here
+            ],
+            "select_any": true,
+            "correct": "<p><span>Nice!</span></p>",
+            "incorrect": "<p>Maybe this will help: Well, the mad sun has escaped- but he's still hot! He opens his mouth and out flows a boiling river.  In the river is some <em>money row</em>-ing a boat.</p>"// no comma here
+        },
+        { // Question 3 - Multiple Choice, Multiple True Answers, Select Any
+            "q": "Who came before William Henry Harrison",
+            "a": [
+                {"option": "Thomas Jefferson",           "correct": false},
+                {"option": "John Tyler",                  "correct": false},
+                {"option": "Martin Van Buren",  "correct": true},
+                {"option": "Millard Fillmore",          "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Brilliant!</span></p>",
+            "incorrect": "<p><span>Not Quite.</span> Picture the hairy <b> van </b></p>" // no comma here
+        },
+        { // Question 4
+            "q": "Who came before and after (in that order) Millard Fillmore?",
+            "a": [
+                {"option": "Tyler, Pierce",    "correct": false},
+                {"option": "Taylor, Pierce",     "correct": true},
+                {"option": "Taylor, Buchannon",      "correct": false},
+                {"option": "Tyler, Buchannon",   "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Holy bananas!</span>  Correct!</p>",
+            "incorrect": "<p><span>Oh no.</span> Sorry. It actually was Taylor and Pierce.</p>" // no comma here
+        },
+        { // Question 5
+            "q": "Who was the 16th president?",
+            "a": [
+                {"option": "Abraham Lincoln",    "correct": true},
+                {"option": "James Madison",    "correct": false},
+                {"option": "Andrew Jackson",    "correct": false},
+                {"option": "Millard Fillmore",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Good Job!</span> </p>",
+            "incorrect": "<p><span>Aww!</span> It's actually Abraham Lincoln</p>" // no comma here
+        } // no comma here
+    ]
+};
