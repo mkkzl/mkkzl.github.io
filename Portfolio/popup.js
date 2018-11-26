3b0d45256c073245218f125192e3bbc23b43eccb
mkkzl <mkozal@nd.edu>
2018-11-25 20:33:31 -0500

    pop up

--- Portfolio/popup.js
+++ Portfolio/popup.js
@@ -1,5 +1,3 @@
-sessionStorage.setItem('firstVisit', '1');
     $("document").ready( function () {
-       if (!sessionStorage.getItem('firstVisit') === "1")
-       {  alert("To navigate the site, click the M logo box!");}
+         alert("To navigate the site, click the M logo box!");
     });
