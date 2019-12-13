ef54bf0ddee2a655d0548fc7f362131c5b092bb6
mkkzl <mkozal@nd.edu>
2019-12-13 15:32:34 -0500

    touchstart

--- canvas/blocks.js
+++ canvas/blocks.js
@@ -19,9 +19,28 @@ window.addEventListener("DOMContentLoaded", function()
 
       var mouseClicked = false;
       document.addEventListener("click", onMouseClick, false);
+      var touchStart = false;
+      document.addEventListener("touchstart", touchStart, false);
   function onMouseClick(){
     context.drawImage(base_image, 0, 0);
     mouseClicked = !mouseClicked;
+
+    var rectnum= Math.floor(Math.random() * 15) + 1;
+
+    while (rectnum > 0){
+      var x= Math.floor(Math.random() * 500) + 1;
+      var y= Math.floor(Math.random() * 347) + 1;
+      var w= Math.floor(Math.random() * (500-x)) + 1;
+      var h= Math.floor(Math.random() * (347-y)) + 1;
+      //console.log(x,y,w,h)
+      context.fillStyle= '#000000'
+      rectnum= rectnum-1
+      context.fillRect(x,y,w,h)
+    }
+  }
+  function onTouchStart(){
+    context.drawImage(base_image, 0, 0);
+    touchStart = !touchStart;
 
     var rectnum= Math.floor(Math.random() * 15) + 1;
 
