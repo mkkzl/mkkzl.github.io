943471f0ea8533c150956bc233a8e5e42ad6cdec
mkkzl <mkozal@nd.edu>
2020-01-15 19:34:26 -0500

    update map points

--- canvas/map/dot.js
+++ canvas/map/dot.js
@@ -1,11 +1,31 @@
 var dots = document.getElementsByClassName('dot');
+for(var d; d<dots.length; d++){
 
-for (var d; d<dots.length; d++){
-  dots[d].onclick()= showData();
+var clicked_id=dots[d].id;
+
+//function showData(clicked_id, clicked_src){
+dots[d].onclick= function() {
+  $.getJSON("map.json", function(json){
+    for (var i = 0; i<json.length; i++){
+      var id= json[i].id;
+      var date= json[i].date;
+      var rate= json[i].rate;
+      var position= json[i].position;
+      var image= json[i].image;
+
+      if(id==clicked_id){
+        var date_place= document.getElementById('date');
+        var rate_place= document.getElementById('rate');
+        var position_place= document.getElementById('postition');
+        var image_place= document.getElementById('day_image');
+
+        date_place.appendChild(document.createTextNode('Date: ',date));
+        rate_place.appendChild(document.createTextNode('Rate: ',rate));
+        position_place.appendChild(document.createTextNode('Position: ',position));
+        image_place.src= image;
+      }
+
 }
-
-function showData(){
-  for (var d; d<dots.length; d++){
-    
-  }
+});
+}
 }
