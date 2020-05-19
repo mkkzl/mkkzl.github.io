31981d596e20f03c38b63b44260e48a62e04b186
mkkzl <mkozal@nd.edu>
2020-05-19 12:38:11 -0400

    update publications

---
+++ canvas/publications/js/functions.js
@@ -0,0 +1,67 @@
+
+function clear(){
+  $( ".slides" ).empty();
+
+}
+function timepassing(){
+  for( var i=1; i<=76; i++){
+ var divframe= document.createElement("div");
+ divframe.className= "mySlides";
+
+ var image= document.createElement("img");
+ image.src= "timepassing/timepassing"+i+".png";
+ // image.style.width= 100%;
+ divframe.appendChild(image);
+
+ document.getElementById("divframe").appendChild( divframe);
+}
+}
+
+function purchistories(){
+  for( var i=1; i<=5; i++){
+ var divframe= document.createElement("div");
+ divframe.className= "mySlides";
+ var image= document.createElement("img");
+ image.src= "purchistories/purchistories"+i+".png";
+
+ divframe.appendChild(image);
+ document.getElementById("divframe").appendChild(divframe);
+}
+
+}
+function openModal() {
+document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+document.getElementById("myModal").style.display = "none";
+clear();
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+var i;
+var slides = document.getElementsByClassName("mySlides");
+//var dots = document.getElementsByClassName("demo");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+}
+//  for (i = 0; i < dots.length; i++) {
+//    dots[i].className = dots[i].className.replace(" active", "");
+//  }
+slides[slideIndex-1].style.display = "block";
+
+//dots[slideIndex-1].className += " active";
+}
---
+++ canvas/publications/js/functionsnew.js
@@ -0,0 +1,69 @@
+function clear(){
+
+  console.log("empty");
+}
+
+function timepassing(){
+  for( var i=1; i<=76; i++){
+ var divframe= document.createElement("div");
+ divframe.className= "mySlides";
+
+ var image= document.createElement("img");
+ image.src= "timepassing/timepassing"+i+".png";
+ image.style.width= "100%";
+ divframe.appendChild(image);
+
+ document.getElementById("divframe").appendChild( divframe);
+ document.getElementById("divframe").style.background= "";
+}
+}
+
+function purchistories(){
+  for( var i=1; i<=5; i++){
+ var divframe= document.createElement("div");
+ divframe.className= "mySlides";
+ var image= document.createElement("img");
+ image.src= "purchistories/purchistories"+i+".png";
+image.style.height= "90vmin";
+ divframe.appendChild(image);
+ document.getElementById("divframe").appendChild(divframe);
+  document.getElementById("divframe").style.background= "#fefefe";
+}
+}
+
+function openModal() {
+ document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+ document.getElementById("myModal").style.display = "none";
+ $( ".slides" ).empty();
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+ showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+ showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+ var i;
+ var slides = document.getElementsByClassName("mySlides");
+ //var dots = document.getElementsByClassName("demo");
+ if (n > slides.length) {slideIndex = 1}
+ if (n < 1) {slideIndex = slides.length}
+ for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+ }
+//  for (i = 0; i < dots.length; i++) {
+ //    dots[i].className = dots[i].className.replace(" active", "");
+//  }
+ slides[slideIndex-1].style.display = "block";
+
+ //dots[slideIndex-1].className += " active";
+}
