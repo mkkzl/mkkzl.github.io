3cc9089d1d5dec163b0dfcbbf7b994c110acd009
mkkzl <mkozal@nd.edu>
2020-05-19 15:20:57 -0400

    remove .html and add icon

--- canvas/publications/js/functions.js
+++ canvas/publications/js/functions.js
@@ -65,3 +65,21 @@ slides[slideIndex-1].style.display = "block";
 
 //dots[slideIndex-1].className += " active";
 }
+
+function zoomin() {
+  var myImg = document.getElementById("divframe");
+  var currWidth = myImg.clientWidth;
+  if (currWidth == 2500) return false;
+  else {
+    myImg.style.width = (currWidth + 100) + "px";
+  }
+}
+
+function zoomout() {
+  var myImg = document.getElementById("divframe");
+  var currWidth = myImg.clientWidth;
+  if (currWidth == 100) return false;
+  else {
+    myImg.style.width = (currWidth - 100) + "px";
+  }
+}
--- canvas/publications/js/functionsnew.js
+++ canvas/publications/js/functionsnew.js
@@ -1,3 +1,19 @@
+  var zoom = 1;
+  $(".in").on('click',function() {
+			zoom += 0.1;
+			$('.mySlides').css('zoom', zoom);
+      $('#divframe').css('zoom', zoom);
+      console.log(zoom);
+		}
+  );
+
+  $(".out").on('click',function() {
+  			zoom -= 0.1;
+        $('.mySlides').css('zoom', zoom);
+        $('#divframe').css('zoom', zoom);
+        console.log(zoom);
+  		});
+
 function clear(){
 
   console.log("empty");
@@ -10,7 +26,8 @@ function timepassing(){
 
  var image= document.createElement("img");
  image.src= "timepassing/timepassing"+i+".png";
- image.style.width= "100%";
+ image.style.height= "90vmin";
+ image.className="image";
  divframe.appendChild(image);
 
  document.getElementById("divframe").appendChild( divframe);
@@ -25,6 +42,7 @@ function purchistories(){
  var image= document.createElement("img");
  image.src= "purchistories/purchistories"+i+".png";
 image.style.height= "90vmin";
+image.className="image";
  divframe.appendChild(image);
  document.getElementById("divframe").appendChild(divframe);
   document.getElementById("divframe").style.background= "#fefefe";
@@ -64,6 +82,7 @@ function showSlides(n) {
  //    dots[i].className = dots[i].className.replace(" active", "");
 //  }
  slides[slideIndex-1].style.display = "block";
+
 
  //dots[slideIndex-1].className += " active";
 }
